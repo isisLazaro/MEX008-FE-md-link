aaebef188ba6af33f3baee3f797ab0cdbd9957ed
Isis Lázaro <isislazaro@gmail.com>
2019-08-30 01:30:40 -0500

    lee comando y lo valida o muestra mensaje de error

--- index.js
+++ index.js
@@ -5,5 +5,22 @@
 module.exports = () => {
   console.log('Welcome to the outside!')
 }; */
+//console.log(process.argv);
 
-console.log("Hello world");
+const path = require('path');
+const chalk = require('chalk');
+
+if (process.argv.length <= 2 || process.argv.length > 5) {
+  console.log(`${chalk.red.inverse('ERROR')}\nUsage must be:\nmd-links path-to-file\nmd-links path-to-file --stats \nmd-links path-to-file --validate \nmd-links path-to-file --stats --validate`);
+  process.exit(-1);
+}
+[pathProcessExe, pahtJsExe, ...args] = process.argv;
+console.log(args);
+//console.log(path.basename(args[0]));
+
+/* TODO:
+  Comprobar que se escribió 1 argumento
+  TODO: Comprobar que es una ruta
+  TODO: Comprobar si es una ruta absoluta
+  TODO: Si es relativa, resolver? cambiar absoluta? 
+  commander, minimist*/
